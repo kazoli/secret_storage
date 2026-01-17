560efa4870e3540216af629b2792476722222ab7
KAZOLI <karolyizoltan.1@gmail.com>
2026-01-17 20:06:29 +0100

    fix: revert hashrouter to browserrouter

--- src/providers/RouterProvider/RouterProvider.tsx
+++ src/providers/RouterProvider/RouterProvider.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
 
 import List from '../../pages/List';
 import Login from '../../pages/Login';
